28cee384b3f1613c07cf4f56b7ffc7eb60e90e1d
Jonathan Martz <support@habit-streak.app>
2025-04-22 20:14:57 +0200

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+    <header class="mx-auto max-w-6xl flex justify-between">
+        <section class="logo">
+            <span>Hetzner.app</span>
+        </section>
+    </header>
+    <main class="mx-auto min-h-screen max-w-6xl">
+        <div class="mx-3 my-3 lg:mx-0">
+            <section role="alert" class="alert alert-warning">
+            <p class="font-bold text-center block">
+                Diese ist keine offizielle App von Hetzner sondern eine App von JMSE zur Steuerung von Hetzner
+                via API.
+            </p>
+        </section>
+        </div>
+        <slot />
+    </main>
+    <footer class="mx-auto max-w-6xl">
+        Footer
+    </footer>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <section class="page bg-white text-black px-3 py-3">
+        Hallo Welt
+    </section>
+</template>

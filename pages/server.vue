9c2d4f3d052955f9049725643c7e31cb7060de02
Jonathan Martz <support@habit-streak.app>
2025-04-24 19:50:48 +0200

    add server view page

---
+++ pages/server.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    Server Page
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useLocalStorage } from '@vueuse/core';
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+const router = useRouter();
+const token = useLocalStorage('token', '', {})
+
+onMounted(() => {
+  if (!route.query.id) {
+    router.push('/servers');
+  }
+  if (token.value == '') {
+    router.push('/login');
+  }
+});
+</script>
+
+<style></style>

40c75dbb5582368bff042489e816e7d2787b6ac5
Jonathan Martz <support@habit-streak.app>
2025-04-27 21:54:08 +0200

    start with hero

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,25 @@
 <template>
     <section class="page mx-3 lg:mx-auto bg-white text-black max-w-6xl px-3 py-3">
-        Startseite mit Hero
+        <div class="divider divider-primary">Willkommen auf der Startseite</div>
+        <img src="https://place-hold.it/640x360?text=Hero&fontsize=30" alt="">
+
+        <section class="grid grid-cols-6 my-3 gap-y-3">
+            <div class="col-span-6">
+                <a href="/über-uns" class="btn btn-neutral btn-block text-center">
+                    -> skeptische Kunden <- 
+                </a>
+            </div>
+            <div class="col-span-6 md:col-span-3">
+                <a href="/login" class="btn btn-secondary btn-block text-center">
+                    Hetzner Kunden mit Token ->
+                </a>
+            </div>
+            <div class="col-span-6 md:col-span-3">
+                <a href="/servers" class="btn btn-primary btn-block text-center">
+                    <- Kunden mit eingebene Token
+                </a>
+            </div>
+        </section>
     </section>
 </template>
 
---
+++ pages/über-uns.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="page mx-3 lg:mx-auto bg-white text-black max-w-6xl px-3 py-3">
+    <h3 class="font-bold text-lg flex justify-between">
+      <span>Herzlichen Glückwunsch, du das den Security Award gewonnen.</span>
+    </h3>
+    <p class="text-sm">Das bedeutet du bist skeptisch ob du uns trauen kannst ?</p>
+    <br>
+    <h3 class="font-bold text-lg flex justify-between">
+      <span>Im Code nachnachschauen</span>
+    </h3>
+    <p class="text-sm">
+      Hier ist deine Möglichkeit selber im Code nachzuschauen.
+      <a href="https://github.com/HetznerApp/nuxt-hetzner-app" target="_blank" class="text-blue-600 font-bold"
+        rel="noopener noreferrer">Github - Code</a> Du kannst zu Beispiel nachschauen wie wir mit dem Token umgehen.
+    </p>
+    <br>
+    <h3 class="font-bold text-lg flex justify-between">
+      <span>Wir erklären dir was passiert.</span>
+    </h3>
+    <p class="text-sm">
+      <span class="font-bold">Token</span> - wir speichern diesen lokal im Locale Storage und senden ihn ne zu unserem
+      Server.<br>
+      <span class="font-bold">Token</span> - der Token wird aufgrund der https regeln die für unsere TLD exestiert immer
+      verschlüsselt an Hetzner übertragen.
+    </p>
+  </section>
+</template>
+
+<script lang="ts" setup>
+
+</script>
